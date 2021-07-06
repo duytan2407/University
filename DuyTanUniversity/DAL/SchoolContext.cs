cbe4c7c32e1454a74032edf492950ad943e90d1f
Tân Duy <duytan240700@outlook.com>
2021-07-06 12:42:37 +0700

    Add project files.

---
+++ DuyTanUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using DuyTanUniversity.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DuyTanUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
