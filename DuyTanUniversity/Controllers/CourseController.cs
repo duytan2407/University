b99a53a5564801e9f454927b5a6d5e4821cf55ef
Tân Duy <duytan240700@outlook.com>
2021-07-06 15:03:43 +0700

    toi 170

---
+++ DuyTanUniversity/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using DuyTanUniversity.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace DuyTanUniversity.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        private SchoolContext db = new SchoolContext();
+        public ActionResult Index()
+        {
+            var courses = db.Courses.Include(c => c.Department);
+            return View(courses.ToList());
+        }
+    }
+}
--- DuyTanUniversity/Models/Student.cs
+++ DuyTanUniversity/Models/Student.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DuyTanUniversity.Models
 {
     public class Student
     {
+       
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
         public string Secret { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return this.LastName + ", " + this.FirstMidName;
+            }
+        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+       
+
     }
 }
